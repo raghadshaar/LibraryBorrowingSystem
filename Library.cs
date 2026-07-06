260414bdda8a9e455bcff0f89f1e6052f7c2af77
raghadshaar <raghadshaar8@gmail.com>
2026-07-06 19:53:28 +0300

    feat: add borrow,return books methods to library class

--- Library.cs
+++ Library.cs
@@ -1,23 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LibrarySystem
+﻿namespace LibrarySystem
 {
     public class Library
     {
-        private List<Book> books = new List<Book>();
+        private readonly List<Book> books = new();
+        private readonly MemberRepository memberRepository = new();
 
         public void AddBook(Book book)
         {
-
-
             if (book != null && !books.Any(b => b.Id == book.Id))
                 books.Add(book);
-
-
         }
 
         public void RemoveBook(int id)
@@ -25,18 +16,69 @@ namespace LibrarySystem
             Book? book = books.FirstOrDefault(b => b.Id == id);
 
             if (book != null)
-            {
                 books.Remove(book);
-            }
         }
 
-        public List<Book> GetAvailableBooks()
+        public IReadOnlyList<Book> GetAvailableBooks()
         {
             return books
                 .Where(b => b.AvailabilityStatus == AvailabilityStatus.Available)
                 .ToList();
         }
 
+        public void RegisterMember(Member member)
+        {
+            memberRepository.AddMember(member);
+        }
+
+        public IReadOnlyList<Member> GetMembers()
+        {
+            return memberRepository.GetMembers();
+        }
+
+
+        public bool BorrowBook(int memberId, int bookId)
+        {
+            Member? member = memberRepository
+                .GetMembers()
+                .FirstOrDefault(m => m.Id == memberId);
+
+            if (member == null)
+                return false;
+
+            Book? book = books.FirstOrDefault(b => b.Id == bookId);
+
+            if (book == null)
+                return false;
+
+            if (book.AvailabilityStatus == AvailabilityStatus.Borrowed)
+                return false;
+
+            book.AvailabilityStatus = AvailabilityStatus.Borrowed;
+
+            return true;
+        }
+
+        public bool ReturnBook(int memberId, int bookId)
+        {
+            Member? member = memberRepository
+                .GetMembers()
+                .FirstOrDefault(m => m.Id == memberId);
+
+            if (member == null)
+                return false;
+
+            Book? book = books.FirstOrDefault(b => b.Id == bookId);
+
+            if (book == null)
+                return false;
+
+            if (book.AvailabilityStatus == AvailabilityStatus.Available)
+                return false;
+
+            book.AvailabilityStatus = AvailabilityStatus.Available;
+
+            return true;
+        }
     }
 }
-
