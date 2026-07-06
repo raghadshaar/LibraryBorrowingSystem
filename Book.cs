94b3ef4b54aabeea939d593b412edfbff215d3ed
raghadshaar <raghadshaar8@gmail.com>
2026-07-06 19:27:29 +0300

    feat: make book class abstract with 2 classes inherit it ebook physical book

--- Book.cs
+++ Book.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace LibrarySystem
 {
-    internal class Book
+    public abstract class Book
     {
+
+        public int Id { get; set; }
+        public string? Author { get; set; }
+        public string? Title { get; set; }
+        public string? ISBN { get; set; }
+        public AvailabilityStatus AvailabilityStatus { get; set; }
+        public abstract int BorrowingDays { get; }
+
+
     }
+
+    public enum AvailabilityStatus
+    {
+        Available,
+        Borrowed,
+        Reserved,
+        Lost,
+        UnderMaintenance
+    }
+    public class PhysicalBook : Book
+    {
+        public override int BorrowingDays => 14;
+    }
+
+    public class EBook : Book
+    {
+        public override int BorrowingDays => 7;
+    }
+
+
+}
+
 }
